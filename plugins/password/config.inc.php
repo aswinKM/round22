7b3e7b28128a2188de34c1ec4f852ea0b38ce049
aswinKM <aswinmohanan001@gmail.com>
2024-04-10 20:28:05 +0530

    Update config.inc.php

--- plugins/password/config.inc.php
+++ plugins/password/config.inc.php
@@ -1,1 +1,26 @@
-/etc/roundcube/plugins/password/config.inc.php
+<?php
+
+// Password Plugin options
+// -----------------------
+// A driver to use for password change. Default: "sql".
+// See README file for list of supported driver names.
+$config["password_driver"] = "cloudstick";
+
+// Require the new password to be a certain length.
+// set to blank to allow passwords of any length
+$config["password_minimum_length"] = 8;
+
+// Require the new password to contain a letter and punctuation character
+// Change to false to remove this check.
+$config["password_require_nonalpha"] = false;
+
+// Enables logging of password changes into logs/password
+$config["password_log"] = false;
+
+// Comma-separated list of login exceptions for which password change
+// will be not available (no Password tab in Settings)
+$config["password_login_exceptions"] = null;
+
+// By default domains in variables are using unicode.
+// Enable this option to use punycoded names
+$config["password_idn_ascii"] = false;

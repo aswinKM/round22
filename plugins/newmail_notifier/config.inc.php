d22def08da5a5a954225d8bd6159a080fe3703fc
aswinKM <aswinmohanan001@gmail.com>
2024-04-10 20:29:18 +0530

    Update config.inc.php

--- plugins/newmail_notifier/config.inc.php
+++ plugins/newmail_notifier/config.inc.php
@@ -1,1 +1,15 @@
-/etc/roundcube/plugins/newmail_notifier/config.inc.php
+<?php
+
+// Enables basic notification
+$config["newmail_notifier_basic"] = true;
+
+// Enables sound notification
+$config["newmail_notifier_sound"] = false;
+
+// Enables desktop notification
+$config["newmail_notifier_desktop"] = false;
+
+// Desktop notification close timeout in seconds
+$config["newmail_notifier_desktop_timeout"] = 5;
+
+?>
